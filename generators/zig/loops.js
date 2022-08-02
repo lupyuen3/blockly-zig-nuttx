7734362ef3e67c99fbe08cd4e7a44ee00e88305a
Lee Lup Yuen <luppy@appkaki.com>
2022-08-02 19:28:25 +0800

    Generate Repeat Loop

--- generators/zig/loops.js
+++ generators/zig/loops.js
@@ -35,8 +35,12 @@ Zig['controls_repeat_ext'] = function(block) {
     endVar = Zig.nameDB_.getDistinctName('repeat_end', NameType.VARIABLE);
     code += 'var ' + endVar + ' = ' + repeats + ';\n';
   }
-  code += 'for (int ' + loopVar + ' = 0; ' + loopVar + ' < ' + endVar + '; ' +
-      loopVar + '++) {\n' + branch + '}\n';
+  code += `var ${loopVar}: usize = 0;\n`;
+  code += [
+    `while (${loopVar} < ${endVar}) : (${loopVar} += 1) {\n`,
+    branch,
+    '}\n'
+  ].join('');
   return code;
 };
 
