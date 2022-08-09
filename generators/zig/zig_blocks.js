de208c3e42343e61eb5f5fb7f36847c7c0c90615
Lee Lup Yuen <luppy@appkaki.com>
2022-08-09 18:22:36 +0800

    Add custom blocks

---
+++ generators/zig/zig_blocks.js
@@ -0,0 +1,51 @@
+/// Custom blocks exported from Block Exporter based on zig_library.xml.
+/// See zig_functions.js for Code Generator Functions.
+var zig_blocks =
+// Begin Block Exporter
+[{
+  "type": "every",
+  "message0": "every %1 seconds %2 %3",
+  "args0": [
+    {
+      "type": "field_number",
+      "name": "DURATION",
+      "value": 10,
+      "min": 0
+    },
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_statement",
+      "name": "STMTS"
+    }
+  ],
+  "colour": 120,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "field",
+  "message0": "field %1 %2 value %3",
+  "args0": [
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "field_input",
+      "name": "NAME",
+      "text": "name"
+    },
+    {
+      "type": "input_value",
+      "name": "name"
+    }
+  ],
+  "inputsInline": true,
+  "output": null,
+  "colour": 120,
+  "tooltip": "",
+  "helpUrl": ""
+}]
+// End Block Exporter
+;
